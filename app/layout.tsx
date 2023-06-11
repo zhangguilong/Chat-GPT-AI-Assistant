3ce26250276d499f329457b48361edbf190d4745
zhangguilong <zhangguilong99@163.com>
2023-06-11 16:01:41 +0800

    Update layout.tsx
    修改title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "智能服务助手",
+  description: "你的定制人工智能机器人",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -19,7 +19,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "智能服务助手",
     statusBarStyle: "default",
   },
 };
